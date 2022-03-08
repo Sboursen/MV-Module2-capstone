c6913a4cbaa6c2af66b3d652bf7a598358ad03f0
sboursen <dev.sboursen@gmail.com>
2022-03-08 11:35:08 +0100

    Fix not being able to fetch data issu

--- src/modules/cars-api.js
+++ src/modules/cars-api.js
@@ -3,21 +3,31 @@ export default class CarsApi {
     url = 'https://private-anon-66fbb79774-carsapi1.apiary-mock.com/',
   ) {
     this.rootEndpoint = url;
-    this.allCarsEndpoint = `${url}cars/`;
+    this.allCarsEndpoint = `${url}cars`;
   }
 
   #getCarEndpointFromId = (id) =>
-    `${this.allCarsEndpoint}${id}/`;
+    `${this.allCarsEndpoint}/${id}`;
 
-  getAllCars = async (id) => {
-    const url = this.#getCarEndpointFromId(id);
-    const response = await fetch(url);
+  getAllCars = async (url = this.allCarsEndpoint) => {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     const data = await response.json();
     return data;
   };
 
-  getCarById = async (url = this.allCarsEndpoint) => {
-    const response = await fetch(url);
+  getCarById = async (id) => {
+    const url = this.#getCarEndpointFromId(id);
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
     const data = await response.json();
     return data;
   };
