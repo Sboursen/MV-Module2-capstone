e803d6d69be6a4a35d2b410df00d7d2e2671f87b
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-03-08 17:34:01 +0200

    Create the popup interface

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './style.css';
+
+const open = document.querySelector('#OpenModal');
+const modalContainer = document.querySelector('#modal-container');
+const modal = document.querySelector('#modal');
