66a283091151a80bed9fe52f992766f7487ce5d3
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-10 10:42:59 +0000

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {
 } from './modules/home/home-app';
 import showCommentModal from './modules/comments/comments-app';
 import showReservationModal from './modules/reservations/reservation-app';
-import { getDataFromInvolvementAPI } from './modules/reservations/involvementAPI';
+import { setReservedToAPI, getDataFromInvolvementAPI } from './modules/reservations/involvementAPI';
 
 const homeApp = new HomeApplication();
 
@@ -32,9 +32,8 @@ homeApp.initialize().then(() => {
   );
 
   reservationButtons.forEach((btn) => btn.addEventListener('click', showReservationModal));
+  getDataFromInvolvementAPI('item1');
+  setReservedToAPI();
 
   registerHomeEvents();
 });
-
-getDataFromInvolvementAPI('item1');
-// setReservedToAPI('item1', 'Jerry', '2022/03/12', '2022/03/13');
