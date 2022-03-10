4cce7f58ba50f840c9f926ff77d88055594f83f4
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-10 10:42:09 +0000

    Update involvementAPi file

---
+++ src/modules/reservations/involvementAPI.js
@@ -0,0 +1,36 @@
+import reservationCounter from './reservation-counter';
+
+const setReservedToAPI = async (item_id, username, date_start, date_end) => {
+  const data = {
+    item_id, username, date_start, date_end,
+  };
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/2C4jJFKraIvaxjrsdqH5/reservations', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
+
+const getDataFromInvolvementAPI = async (item_id) => {
+  const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/2C4jJFKraIvaxjrsdqH5/reservations?item_id=${item_id}`;
+  const reservationsData = await fetch(url).then((response) => response.json());
+
+  const reservationsList = reservationsData;
+  const counter = reservationCounter(reservationsList);
+
+  console.log(item_id);
+  console.log(counter);
+
+  const counterContainer = document.querySelector('#counter-span');
+  counterContainer.innerHTML = `(${counter})`;
+
+  const displayReserves = document.querySelector('.reserved-car');
+  displayReserves.innerHTML += ` 
+  ${reservationsList.date_start} - ${reservationsList.date_end} by ${reservationsList.username};
+ `;
+};
+
+export { setReservedToAPI, getDataFromInvolvementAPI };
